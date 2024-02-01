aca00db5e56ad4b61d8455c336368eac1e62ca6b
Ruben <55102143+boulomiel@users.noreply.github.com>
2024-02-01 14:41:48 +0200

    Initial Commit

---
+++ PickPick/PickPickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickPickApp.swift
+//  PickPick
+//
+//  Created by Ruben Mimoun on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PickPickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
